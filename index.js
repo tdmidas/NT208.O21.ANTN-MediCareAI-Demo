a43cf8ed29ff88a6eaa575f1c3868c4f9ef913dc
tdmidas <tranduongminhdai@gmail.com>
2024-03-11 13:17:19 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const list = document.querySelectorAll(".list");
-function activeLink() {
-	list.forEach((item) => item.classList.remove("active"));
-	this.classList.add("active");
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", () => {
+	sidebar.classList.toggle("open");
+	menuBtnChange(); //calling the function(optional)
+});
+
+searchBtn.addEventListener("click", () => {
+	// Sidebar open when you click on the search iocn
+	sidebar.classList.toggle("open");
+	menuBtnChange(); //calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+	if (sidebar.classList.contains("open")) {
+		closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+	} else {
+		closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+	}
 }
-list.forEach((item) => ["click", "mouseover"].forEach((evt) => item.addEventListener(evt, activeLink)));
